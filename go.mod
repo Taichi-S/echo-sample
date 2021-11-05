f89a055ac573ee5ed5fc016bda9de22e047af7f7
Taichi-S <sta10129@gmail.com>
2021-11-05 14:02:07 +0900

    implement CRUD Recipe for echo

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module myapp
 
 go 1.15
 
-require github.com/lib/pq v1.10.3
+require (
+	github.com/labstack/echo/v4 v4.6.1
+	github.com/lib/pq v1.10.3
+)
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"database/sql" // In most cases clients will use the database/sql package instead of using pq package directly.
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq" //Import the Postgres Driver．With the driver imported, you’ll start writing code to access the database.
 )
 
@@ -19,8 +23,10 @@ type Album struct { // use this to hold row data returned from the query
 var db *sql.DB // productionではグローバル変数は非推奨
 
 func main() {
+	// connStr := "host=localhost user=ichi dbname=ichi sslmode=disable"
 	var err error
 	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	// db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err) // In production code, you’ll want to handle errors in a more graceful way.
 	}
@@ -31,75 +37,105 @@ func main() {
 	}
 	fmt.Println("Connected!")
 
-	albums, err := albumsByArtist("John Coltrane")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Albums found: %v\n", albums)
+	e := echo.New()
 
-	// Hard-code ID 2 here to test the query.
-	alb, err := albumByID(2)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Album found: %v\n", alb)
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	albID, err := addAlbum(Album{
-		Title:  "The Modern Sound of Betty Carter",
-		Artist: "Betty Carter",
-		Price:  49.99,
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("ID of added album: %v\n", albID)
+
+	e.GET("/albums", getAllAlbums)
+	e.POST("/albums", createAlbum)
+	e.GET("/album/:id", getAlbum)
+	e.PUT("/albums/:id", updateAlbum)
+	e.DELETE("/albums/:id", deleteAlbum)
+
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
 
-// albumsByArtist queries for albums that have the specified artist name.
-func albumsByArtist(name string) ([]Album, error) {
-	// An albums slice to hold data from returned rows.
+func getAllAlbums(c echo.Context) error {
 	var albums []Album
 
-	rows, err := db.Query("SELECT * FROM album WHERE artist = $1", name) // By separating the SQL statement from parameter values (rather than concatenating them with, say, fmt.Sprintf), you enable the database/sql package to send the values separate from the SQL text, removing any SQL injection risk.すべての検索クエリにはなるべくデータベースが提供するパラメータ化検索インターフェースを使用する。
+	rows, err := db.Query("SELECT * FROM album")
 	if err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		return fmt.Errorf("getAllAlbums: %v", err)
 	}
+
 	defer rows.Close()
-	// Loop through rows, using Scan to assign column data to struct fields.
 	for rows.Next() {
 		var alb Album
 		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil { //Scan takes a list of pointers to Go values
-			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+			return fmt.Errorf("getAllAlbums: %v", err)
 		}
 		albums = append(albums, alb)
 	}
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		return fmt.Errorf("getAllAlbums: %v", err)
 	}
-	return albums, nil
+
+	return c.JSON(http.StatusOK, albums)
 }
 
 // albumByID queries for the album with the specified ID.
-func albumByID(id int64) (Album, error) {
+func getAlbum(c echo.Context) error {
 	// An album to hold data from the returned row.
 	var alb Album
+	id, _ := strconv.Atoi(c.Param("id"))
 
 	row := db.QueryRow("SELECT * FROM album WHERE id = $1", id) //QueryRow executes a query that is expected to return at most one row. QueryRow always returns a non-nil value.Errors are deferred until Row's Scan method is called.
 	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
 		if err == sql.ErrNoRows { //sql.ErrNoRows indicates that the query returned no rows
-			return alb, fmt.Errorf("albumsById %d: no such album", id)
+			return fmt.Errorf("albumsById %d: no such album", id)
 		}
-		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+		return fmt.Errorf("albumsById %d: %v", id, err)
 	}
-	return alb, nil
+
+	return c.JSON(http.StatusOK, alb)
 }
 
-// addAlbum adds the specified album to the database,
-// returning the album ID of the new entry
-func addAlbum(alb Album) (int64, error) {
+func createAlbum(c echo.Context) error {
 	var lastInsertId int64
-	if err := db.QueryRow("INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id", alb.Title, alb.Artist, alb.Price).Scan(&lastInsertId); err != nil {
-		return 0, fmt.Errorf("addAlbum: %v", err)
+	a := new(Album)
+	if err := c.Bind(a); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	return lastInsertId, nil
+
+	if err := db.QueryRow("INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id", a.Title, a.Artist, a.Price).Scan(&lastInsertId); err != nil {
+		return fmt.Errorf("createAlbum: %v", err)
+	}
+	a.ID = lastInsertId
+
+	return c.JSON(http.StatusCreated, a)
+}
+
+func updateAlbum(c echo.Context) error {
+	a := new(Album)
+	if err := c.Bind(a); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	if _, err := db.Exec("UPDATE album SET title=$1, artist=$2, price=$3 WHERE id=$4", a.Title, a.Artist, a.Price, id); err != nil {
+		return fmt.Errorf("updateAlbum: %v", err)
+	}
+	a.ID = int64(id)
+
+	return c.JSON(http.StatusOK, a)
+}
+
+func deleteAlbum(c echo.Context) error {
+	a := new(Album)
+	if err := c.Bind(a); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	if _, err := db.Exec("DELETE FROM album WHERE id=$1", id); err != nil {
+		return fmt.Errorf("deleteAlbum: %v", err)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
